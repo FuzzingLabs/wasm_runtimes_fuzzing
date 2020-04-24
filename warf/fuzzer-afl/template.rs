35a10c8cbc4791b323c247a9cf654d8abd21520c
Patrick Ventuzelo <ventuzelo.patrick@gmail.com>
2020-04-24 22:32:00 +0200

    cargo fmt

--- warf/fuzzer-afl/template.rs
+++ warf/fuzzer-afl/template.rs
@@ -1,9 +1,10 @@
-#[macro_use] extern crate afl;
+#[macro_use]
+extern crate afl;
 extern crate fuzz_targets_common;
 use fuzz_targets_common::fuzz_###TARGET### as fuzz_target;
 
 fn main() {
-    fuzz!(|data|{
+    fuzz!(|data| {
         fuzz_target(&data);
     });
 }
--- warf/fuzzer-honggfuzz/template.rs
+++ warf/fuzzer-honggfuzz/template.rs
@@ -1,10 +1,11 @@
-#[macro_use] extern crate honggfuzz;
+#[macro_use]
+extern crate honggfuzz;
 extern crate fuzz_targets_common;
 use fuzz_targets_common::fuzz_###TARGET### as fuzz_target;
 
 fn main() {
     loop {
-        fuzz!(|data|{
+        fuzz!(|data| {
             fuzz_target(data);
         })
     }
