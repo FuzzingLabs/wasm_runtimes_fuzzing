26bbd9e584d2fd62f2a8b32a24f457ab748901aa
Patrick Ventuzelo <ventuzelo.patrick@gmail.com>
2020-07-15 15:10:19 +0200

    fix breaking changes wasmparser v0.59

--- warf/targets/src/wasmparser.rs
+++ warf/targets/src/wasmparser.rs
@@ -5,12 +5,11 @@ WASMPARSER:
 
 /// `Fuzzing wasmparser::Parser` and loop to read all module.
 pub fn fuzz_wasmparser_parser(data: &[u8]) -> bool {
-    use wasmparser::{Parser, ParserState, WasmDecoder};
-    let mut parser = Parser::new(data);
-    loop {
-        match *parser.read() {
-            ParserState::Error(..) => return false,
-            ParserState::EndWasm => break,
+    use wasmparser::Parser;
+    let mut res_iter = Parser::new(0).parse_all(&data);
+    for res in res_iter {
+        match res {
+            Err(_) => return false,
             _ => (),
         }
     }
@@ -19,44 +18,25 @@ pub fn fuzz_wasmparser_parser(data: &[u8]) -> bool {
 
 /// Fuzzing `wasmparser::ValidatingParser` and loop to read all module.
 pub fn fuzz_wasmparser_validate(data: &[u8]) -> bool {
-    use wasmparser::{ParserState, ValidatingParser, WasmDecoder};
-    let mut parser = ValidatingParser::new(data, None);
-    loop {
-        match *parser.read() {
-            ParserState::Error(..) => return false,
-            ParserState::EndWasm => break,
-            _ => (),
-        }
-    }
-    true
+    use wasmparser::validate;
+    validate(data).is_ok()
 }
 
 /// Fuzzing `wasmparser::ValidatingParser` with all features enabled and loop to read all module.
 pub fn fuzz_wasmparser_validate_all_feat(data: &[u8]) -> bool {
-    use wasmparser::{
-        OperatorValidatorConfig, ParserState, ValidatingParser, ValidatingParserConfig, WasmDecoder,
-    };
+    use wasmparser::Validator;
 
-    // activate all features
-    let validator_config: Option<ValidatingParserConfig> = Some(ValidatingParserConfig {
-        operator_config: OperatorValidatorConfig {
-            enable_threads: true,
-            enable_reference_types: true,
-            enable_simd: true,
-            enable_bulk_memory: true,
-            enable_multi_value: true,
-            enable_tail_call: true,
-            enable_module_linking: true,
-        },
-    });
+    let mut validator = Validator::new();
 
-    let mut parser = ValidatingParser::new(data, validator_config);
-    loop {
-        match *parser.read() {
-            ParserState::Error(..) => return false,
-            ParserState::EndWasm => break,
-            _ => (),
-        }
-    }
-    true
+    validator.wasm_reference_types(true);
+    validator.wasm_multi_value(true);
+    validator.wasm_threads(true);
+    validator.wasm_simd(true);
+    validator.wasm_module_linking(true);
+    validator.wasm_tail_call(true);
+    validator.wasm_bulk_memory(true);
+    validator.deterministic_only(true);
+
+    // validate
+    validator.validate_all(data).is_ok()
 }
